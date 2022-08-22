8fbb4d3aa6861f8ba5df43199b2e4fcaebcf9f1d
Freddy B <52885763+ingxfm@users.noreply.github.com>
2022-08-22 18:50:59 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+
+
+def clear_screen():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+
+want_a_band_name = True
+while want_a_band_name:
+    clear_screen()
+    print('Welcome to the name band generator.')
+    city_name = input('what is the name of the city you grew up in?')
+
+    pet_name = input('what is your pet\'s name?')
+
+    print(f'Your band\'s name could be {city_name} {pet_name}.\n')
+
+    continue_naming = input('Do you want another option to name your band? Type \'Y\' for yes: ').lower()
+
+    if not continue_naming != 'y':
+        break
+
+
+
+
+
+
+
